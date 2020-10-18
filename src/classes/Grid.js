9b74dd66e766b9bd88e6faf9cce83a2376397337
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-10-18 10:25:59 -0600

    Create Grid class

---
+++ src/classes/Grid.js
@@ -0,0 +1,28 @@
+import Node from './Node';
+
+class Grid {
+    constructor(w, h, startCoord, goalCoord, walls) {
+        this.width = w;
+        this.height = h;
+        this.startNode = new Node(...startCoord);
+        this.goalNode = new Node(...goalCoord);
+        this.visited = new Map();
+        this.walls = new Map();
+        walls.forEach(w => this.walls.set(String(w), new Node(...w)));      
+    }
+    isGoal(node) {
+        return node.isEqual(goalNode);
+    }
+    visit(node) {
+        this.visited.set(node.id, node);
+    }
+    isVisited(node) {
+        return this.visited.has(node.id);
+    }
+    isWall(node) {
+        return this.walls.has(node.id);
+    }
+    validCoord(node) {
+        return node.x > 0 && node.x < this.height && node.y > 0 && node.y < this.width;
+    }
+}

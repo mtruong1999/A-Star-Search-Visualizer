8b219941b924fdd4c8c8e204b1bcae5949e3d7ce
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-10-21 21:32:02 -0600

    Fix typos and add node type checks

--- src/classes/GridClass.js
+++ src/classes/GridClass.js
@@ -11,18 +11,27 @@ class GridClass {
         walls.forEach(w => this.walls.set(String(w), new Node(...w)));
     }
     isGoal(node) {
-        return node.isEqual(goalNode);
+        return node.isEqual(this.goalNode);
     }
     visit(node) {
         this.visited.set(node.id, node);
     }
     isVisited(node) {
+        if (Array.isArray(node)) {
+            return this.visited.has(node.toString());
+        }
         return this.visited.has(node.id);
     }
     isWall(node) {
+        if (Array.isArray(node)) {
+            return this.walls.has(node.toString());
+        }
         return this.walls.has(node.id);
     }
     validCoord(node) {
+        if (Array.isArray(node)) {
+            return node[0] > 0 && node[0] < this.height && node[1] > 0 && node[1] < this.width;
+        }
         return node.x > 0 && node.x < this.height && node.y > 0 && node.y < this.width;
     }
 }
